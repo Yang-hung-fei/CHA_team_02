d462e5d613107d1843d5d771b8adff00b289751d
Jorden <bacteriadark@gmail.com>
2023-07-28 16:46:11 +0800

    test1

--- src/main/java/Jorden.java
+++ src/main/java/Jorden.java
@@ -1,8 +1,8 @@
 
-public class Yunyi {
+public class Jorden {
 
 	public static void main(String[] args) {
-		System.out.println("開發順順利利,先求有再求好!");
-		System.out.println("開發順順利利,先求有再求好!");
+		System.out.println("!");
+		System.out.println("!");
 	}
 }
